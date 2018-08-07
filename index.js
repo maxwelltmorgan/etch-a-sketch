a6a1674a9217a0c5ec90e1e806fc2274b2a34750
Maxwell Morgan <maxwelltmorgan@gmail.com>
2018-08-06 23:52:39 -0600

    added restart function to clear grid

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const inner = document.querySelector('#inner');
 
-const setGrid = (n) => {
-    for(var i = 0; i < n; i++){
-        for(var j = 0; j < n; j++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.style.width = `${(695/80) + 'px'}`;
-        square.style.height = `${(390/80) + 'px'}`;
-        inner.appendChild(square);
+let squaresPerSide = 16;
+
+const setGrid = (squaresPerSide) => {
+
+    if(squaresPerSide > 0 && squaresPerSide <= 100){
+        for(var i = 0; i < squaresPerSide; i++){
+            for(var j = 0; j < squaresPerSide; j++) {
+                const square = document.createElement('div');
+                square.classList.add('square');
+                square.style.width = `${(695/squaresPerSide) + 'px'}`;
+                square.style.height = `${(390/squaresPerSide) + 'px'}`;
+                inner.appendChild(square);
+            };
         };
+
+        let squares = document.querySelectorAll('.square');
+
+        squares.forEach((square) => {
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = 'black';
+            })
+        });
+    } else {
+        alert('out of range.  please select # between 1-100')
     };
 };
-setGrid(80);
+setGrid(squaresPerSide);
 
-var squares = document.querySelectorAll('.square');
+const restart = () => {
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.remove());
+    let squaresPerSide = prompt('enter # of squares per axis (range 1-100)');
+    setGrid(squaresPerSide);
 
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'black';
-    })
-});
+}
