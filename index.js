700c86cd0bd3066551f6a61dc6142bbf959c1cd0
Maxwell Morgan <maxwelltmorgan@gmail.com>
2018-08-07 01:27:54 -0600

    added rainbow option

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const inner = document.querySelector('#inner');
 
 let squaresPerSide = 16;
 
+const black = 'black';
+const rainbow = ['violet', 'indigo', 'blue', 'limegreen', 'yellow', 'orange', 'red'];
+const shades = ['#cccccc', '#b2b2b2', '#999999', '#7f7f7f', '#666666', '#4c4c4c', '#323232', '#191919', '#000000'];
+
+
+
+const setColor = () => {
+
+    return `${rainbow[Math.floor(Math.random() * 7)]}`
+//return `${black}`;
+};
+
 const setGrid = (squaresPerSide) => {
 
     if(squaresPerSide > 0 && squaresPerSide <= 100){
@@ -19,8 +31,8 @@ const setGrid = (squaresPerSide) => {
 
         squares.forEach((square) => {
             square.addEventListener('mouseover', () => {
-                square.style.backgroundColor = 'black';
-            })
+                square.style.backgroundColor = setColor();
+            });
         });
     } else {
         alert('out of range.  please select # between 1-100')
